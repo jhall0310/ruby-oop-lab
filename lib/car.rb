3154ad853c55e2fb7d719d226583d1ad3463944f
jhall0310 <jonathanphall@me.com>
2016-12-07 15:54:43 -0800

    Project completed

--- lib/car.rb
+++ lib/car.rb
@@ -1,5 +1,15 @@
 class Car
+  attr_accessor :speed
+    @@count = 0
 
+    def initialize
+      @speed = 0
+
+    end
+
+    def accelerate(change)
+      @speed += change
+    end
   # write Car class code here
 
 end
--- lib/race.rb
+++ lib/race.rb
@@ -2,6 +2,38 @@ require_relative "car"
 
 class Race
 
+  def initialize
+    @cars = [Car.new, Car.new]
+    random = Random.new
+
+    @cars[0].accelerate(random.rand(100))
+    @cars[1].accelerate(random.rand(100))
+
+  end
+
+def cars
+
+  @cars
+
+end
+
+
   # write Race class code here
+  def winner
+    if cars[0].speed > cars[1].speed
+      cars[0]
+    else
+      cars[1]
+    end
+  end
+
+  def loser
+   if winner == cars[0]
+     cars[1]
+   else
+     cars[0]
+   end
+ end
+
 
 end
